be4905024a2442a672780061c6f5148e7a6730ed
Daniel <dfb1001@alu.ubu.es>
2024-03-11 16:05:54 +0100

    Se crea un nuevo método en HomeController para mapear el json y luego mapear el archivo cuestionario.html con el contenido

--- javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/controller/HomeController.java
+++ javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,10 +16,38 @@ import tfg.daniel.riskreal.riskrealApp.model.Cuestionario;
 @Controller
 public class HomeController {
 	
+	/**
+	 * Página cuestionario.html
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/cuestionario")
+	public String mostrarCuestionario(Model model) {
+		
+		// Obtenemos el objeto del json.
+		Cuestionario cuestionario = getCuestionario();
+		
+		// Lo cargamos en el modelo
+		model.addAttribute("cuestionario", cuestionario);
+		
+		return "cuestionario";
+	}
+	
+	/**
+	 * Página inicial home.html
+	 * @return
+	 */
 	@GetMapping("/")
 	public String inicio() {
 		
-		
+		return "home"; 
+	}
+	
+	/**
+	 * Método para obtener el cuestionario del json
+	 * @return
+	 */
+	private Cuestionario getCuestionario() {
 		// create Object Mapper
 		ObjectMapper mapper = new ObjectMapper();
 		
@@ -30,16 +59,13 @@ public class HomeController {
 			ClassPathResource staticDataResource = new ClassPathResource("schema_cuestionario_v1.json");
 			File json = staticDataResource.getFile();
 		    quest = mapper.readValue(json, Cuestionario.class);
+		    
 
 		} catch (IOException e) {
 		    e.printStackTrace();
 		}
 		
-		if (quest == null) {
-			System.out.println("No se ha cargado el json");
-		}
-		
-		return "home"; 
+		return quest;
 	}
 }
 
