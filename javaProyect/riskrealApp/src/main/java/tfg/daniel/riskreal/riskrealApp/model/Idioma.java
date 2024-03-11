1598b5da6dab720259b6e2412046f35874eeaed5
Daniel <dfb1001@alu.ubu.es>
2024-03-11 12:14:56 +0100

    Se incluye una clase Enum Idioma con los posibles idiomas de los cuestionarios

--- javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/model/Idioma.java
+++ javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/model/Idioma.java
@@ -1,5 +1,12 @@
 package tfg.daniel.riskreal.riskrealApp.model;
 
+/**
+ * Enum Idioma.
+ * 
+ * Contiene los posibles idiomas de los cuestionarios
+ * 
+ */
+
 public enum Idioma {
 
 	SPA,ENG,ITA,FRA,GER,POR
