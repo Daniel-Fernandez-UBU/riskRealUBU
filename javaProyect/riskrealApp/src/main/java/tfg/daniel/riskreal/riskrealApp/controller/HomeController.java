3eac795dc341fe3f0c24a82a5f885c712c4c18f9
Daniel <dfb1001@alu.ubu.es>
2024-03-24 12:35:38 +0100

    Implementada, de forma muy sencilla, la navegabilidad entre las preguntas

--- javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/controller/HomeController.java
+++ javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -15,6 +16,8 @@ import tfg.daniel.riskreal.riskrealApp.model.Cuestionario;
 
 @Controller
 public class HomeController {
+	
+	private int indice = 0;
 	
 	/**
 	 * Página cuestionario.html
@@ -24,14 +27,42 @@ public class HomeController {
 	@GetMapping("/cuestionario")
 	public String mostrarCuestionario(Model model) {
 		
+		
+		
 		// Obtenemos el objeto del json.
 		Cuestionario cuestionario = getCuestionario();
 		
 		// Lo cargamos en el modelo
 		model.addAttribute("cuestionario", cuestionario);
+		model.addAttribute("indice", getIndex());
+		model.addAttribute("upIndex", updateIndex());
+		model.addAttribute("dwIndex", removeIndex());
 		
 		return "cuestionario";
 	}
+	
+    // Controlador para manejar la solicitud POST
+    @PostMapping("/")
+    public String upIndex(Model model) {
+        // Lógica para actualizar el índice aquí
+		model.addAttribute("upIndex", updateIndex());
+
+        // Después de realizar la operación, redirige de nuevo a la misma página
+        return "redirect:/cuestionario"; // Redirige a la misma página
+    }
+    
+    // Controlador para manejar la solicitud POST
+    @PostMapping("/cuestionario")
+    public String dwIndex(Model model) {
+        // Lógica para actualizar el índice aquí
+		model.addAttribute("dwIndex", removeIndex());
+
+        // Después de realizar la operación, redirige de nuevo a la misma página
+        return "redirect:/cuestionario"; // Redirige a la misma página
+    }
+    
+    
+    
 	
 	/**
 	 * Página inicial home.html
@@ -66,6 +97,20 @@ public class HomeController {
 		}
 		
 		return quest;
+	}
+	
+	public int updateIndex() {
+		this.indice += 1;
+		return this.indice;
+	}
+	
+	public int getIndex() {
+		return this.indice;
+	}
+	
+	public int removeIndex() {
+		this.indice -= 1;
+		return this.indice;
 	}
 }
 
