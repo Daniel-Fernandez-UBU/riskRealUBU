dbe2af3a19add606d3f8961471eb607cbd3d0e20
Daniel <dfb1001@alu.ubu.es>
2024-05-04 17:23:26 +0200

    Se configura el inicio de sesión a través de base de datos. Desde ahí también se tiene acceso a los roles

--- javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/config/SecurityConfig.java
+++ javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/config/SecurityConfig.java
@@ -1,11 +1,21 @@
 package tfg.daniel.riskreal.riskrealApp.config;
 
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * SecurityConfig.
+ * 
+ * Class with all the security config related
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -26,6 +36,18 @@ public class SecurityConfig {
 						)
 				.build();
 	}
+    
+    @Bean
+    UserDetailsManager usersCustom(DataSource dataSource) {
+    	
+    	JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    	
+    	users.setUsersByUsernameQuery("select username, password, status from Users u where username=?");
+    	users.setAuthoritiesByUsernameQuery("select username, profile from Profiles p where username=?");
+    	   	
+    	return users;
+    	
+    }
 
 }
 
