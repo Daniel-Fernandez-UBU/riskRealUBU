dbd8527f76ffd62166eda8d5f6507438c84a145d
Daniel <dfb1001@alu.ubu.es>
2024-05-20 20:29:43 +0200

    Incluimos el bean para encriptar contraseñas

--- javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/config/SecurityConfig.java
+++ javaProyect/riskrealApp/src/main/java/tfg/daniel/riskreal/riskrealApp/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -54,6 +56,11 @@ public class SecurityConfig {
     	   	
     	return users;
     	
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
     }
 
 }
